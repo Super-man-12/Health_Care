f3cba91e80520123d099cef473fab032b31f142b
Super-man-12 <abdulhadi122011@gmail.com>
2026-01-04 14:35:33 +0500

    Change app.run to use environment variable for port

--- app.py
+++ app.py
@@ -72,5 +72,5 @@ def handle_translation(data):
 
 # Run locally
 if __name__ == '__main__':
-    # Use socketio.run() for WebSocket support
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
